5bae9461e61943be6e5fd373c0184945a33ebad0
Sergei <logan57logan@gmail.com>
2024-07-05 19:52:04 +0300

    Арифмитические операторы

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('hello moto');
+const width = 10;
+const height = 5;
+const space = width * height;
+const newWidth = width - 4;
+const newWidth2 = width + 4;
+const division = newWidth / newWidth2;
+const volume = 2 ** 3;
+console.log(volume);
+// строки
+const city = 'Москва';
+const street = 'Новослободская';
+console.log(city + ',' + street + ' ' + 5);
